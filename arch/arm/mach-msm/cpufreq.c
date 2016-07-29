9549e922c36174051da5699aeeb15b980eb0f8b2
mostafa-z <mostafazarghami@gmail.com>
2016-07-30 02:15:53 +0430

    CPUFREQ_LIMIT: SQUASH UPDATE TO V 5.1 ... https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/fde4fbd9d513430f64cd29e67ee83daea31ae300 https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/493506387480692bf8a7a2ac0498b699abe074a4 https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/83fecb44d8368bc01ac67cdb455e0ba1838b489a https://github.com/dorimanx/DORIMANX_LG_STOCK_LP_KERNEL/commit/9db9fe5916fcebcad9d5e791b74b768d808fe6ae

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -73,8 +73,12 @@ struct cpufreq_suspend_t {
 static DEFINE_PER_CPU(struct cpufreq_suspend_t, cpufreq_suspend);
 
 #ifdef CONFIG_MSM_CPUFREQ_LIMITER
+<<<<<<< HEAD
 static unsigned int upper_limit_freq[NR_CPUS] = {0, 0, 0, 0};
 static unsigned int lower_limit_freq[NR_CPUS];
+#define CPU_MAX_DEFAULT_FREQ	1190000
+#define CPU_MAX_OC_FREQ		1190000
+#define CPU_MIN_DEFAULT_FREQ	300000
 
 unsigned int get_cpu_min_lock(unsigned int cpu)
 {
@@ -88,7 +92,7 @@ EXPORT_SYMBOL(get_cpu_min_lock);
 void set_cpu_min_lock(unsigned int cpu, int freq)
 {
 	if (cpu >= 0 && cpu < NR_CPUS) {
-		if (freq <= 300000 || freq > 2457600)
+		if (freq <= CPU_MIN_DEFAULT_FREQ || freq > CPU_MAX_OC_FREQ)
 			lower_limit_freq[cpu] = 0;
 		else
 			lower_limit_freq[cpu] = freq;
@@ -108,8 +112,10 @@ EXPORT_SYMBOL(get_max_lock);
 void set_max_lock(unsigned int cpu, unsigned int freq)
 {
 	if (cpu >= 0 && cpu <= NR_CPUS) {
-		if (freq <= 300000 || freq > 2457600)
+		if (freq == 0)
 			upper_limit_freq[cpu] = 0;
+		else if (freq < CPU_MIN_DEFAULT_FREQ || freq > CPU_MAX_OC_FREQ)
+			upper_limit_freq[cpu] = CPU_MAX_DEFAULT_FREQ;
 		else
 			upper_limit_freq[cpu] = freq;
 	}
@@ -338,8 +344,8 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	policy->max = CONFIG_MSM_CPU_FREQ_MAX;
 #else
 #ifdef CONFIG_ARCH_MSM8974
-	policy->max = 2457600;
-	policy->min = 300000;
+	policy->max = CPU_MAX_DEFAULT_FREQ;
+	policy->min = CPU_MIN_DEFAULT_FREQ;
 #endif
 #endif
 
