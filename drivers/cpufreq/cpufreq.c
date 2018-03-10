992683dc9c9c37818d21d52467e11097593fbe2b
infus38 <infus38@gmail.com>
2018-03-10 14:44:49 +0100

    Revert "cpufreq: Move get_cpu_idle_time() to cpufreq.c"
    This reverts commit 6eb7534324b318472312bad216284ac470f0c35b.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -14,11 +14,9 @@
  * published by the Free Software Foundation.
  *
  */
-
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 #include <asm/cputime.h>
 #include <linux/kernel.h>
-#include <linux/kernel_stat.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/notifier.h>
@@ -26,7 +24,6 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/spinlock.h>
-#include <linux/tick.h>
 #include <linux/device.h>
 #include <linux/slab.h>
 #include <linux/cpu.h>
